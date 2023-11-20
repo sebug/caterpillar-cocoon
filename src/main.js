c6374bfec90755289d2d87e97d563f63c9a2405c
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-11-20 11:44:20 +0000

    Adding detector

--- src/main.js
+++ src/main.js
@@ -2,11 +2,54 @@ console.log(window.BarcodeDetector);
 
 const activateCameraButton = document.querySelector('#activate-camera');
 const canvasElement = document.querySelector('#barcode-canvas');
+const ctx = canvasElement.getContext('2d');
 const videoElement = document.querySelector('#barcode-video');
 
-const detectVideo = repeat => {
+let requestId = null;
+let supportedFormats;
+let detector;
 
+const createDetector = async () => {
+    supportedFormats = await BarcodeDetector.getSupportedFormats();
+    detector = new BarcodeDetector({ formats: supportedFormats });
 };
+
+const detect = async source => {
+    let symbols = await detector.detect(source);
+    canvasElement.width = source.naturalWidth || source.videoWidth || source.width;
+    canvasElement.height = source.naturalHeight || source.videoHeight || source.height;
+    ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+    symbols.forEach(symbol => {
+        const lastCornerPoint = symbol.cornerPoints[symbol.cornerPoints.length - 1];
+        ctx.moveTo(lastCornerPoint.x, lastCornerPoint.y);
+        symbol.cornerPoints.forEach(point => ctx.lineTo(point.x, point.y));
+
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = '#00e000ff';
+        ctx.stroke();
+    });
+};
+
+const detectVideo = async repeat => {
+    if (!repeat) {
+        ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    }
+
+    if (typeof repeat === 'undefined') {
+        repeat = true;
+    }
+
+    if (repeat) {
+        await detect(videoElement);
+        requestId = requestAnimationFrame(() => detectVideo(true));
+    } else {
+        cancelAnimationFrame(requestId);
+        requestId = null;
+    }
+};
+
+createDetector();
 
 activateCameraButton.addEventListener('click', async () => {
     let stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: 'environment' }});
